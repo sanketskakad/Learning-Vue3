a3b8da72cad2a2717429f9859e46c00d64bdabb4
Sanket <sanketskakad.other@gmail.com>
2023-02-20 08:44:19 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <div class="card">
+      <div class="card-container">
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 40%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+
+.card-container {
+  padding: 2px 16px;
+}
+</style>
